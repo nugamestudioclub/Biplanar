caca24ed486e4a184e95afc318f1b241473066df
Enator18 <62544405+Enator18@users.noreply.github.com>
2024-10-01 16:49:14 -0400

    added tilemap array

--- variables.s
+++ variables.s
@@ -1,18 +1,19 @@
-.segment "ZEROPAGE"         ; The first page of internal RAM
-    R0:            .res 1   ; Scratch Registers
+.segment "ZEROPAGE"           ; The first page of internal RAM
+    R0:            .res 1     ; Scratch Registers
     R1:            .res 1
     R2:            .res 1
     R3:            .res 1
-    vram_index:    .res 1   ; Current index of the VRAM buffer
-    frame_counter: .res 1   ; General purpose frame counter
-    x_scroll:      .res 2   ; X position of screen scroll (LSB: pixel position, MSB: screen number)
-    y_scroll:      .res 2   ; Y position of screen scroll (LSB: pixel position, MSB: screen number)
-    oam_index:     .res 1   ; Current index of the OAM buffer
-    controller:    .res 1   ; Controller input
-    x_pos:         .res 2   ; X position of the player (LSB: subpixel position, MSB: pixel position)
-    y_pos:         .res 2   ; Y position of the player (LSB: subpixel position, MSB: pixel position)
-    x_vel:         .res 2   ; X velocity of the player (LSB: subpixel velocity, MSB: pixel velocity)
-    y_vel:         .res 2   ; Y velocity of the player (LSB: subpixel velocity, MSB: pixel velocity)
+    vram_index:    .res 1     ; Current index of the VRAM buffer
+    frame_counter: .res 1     ; General purpose frame counter
+    x_scroll:      .res 2     ; X position of screen scroll (LSB: pixel position, MSB: screen number)
+    y_scroll:      .res 2     ; Y position of screen scroll (LSB: pixel position, MSB: screen number)
+    oam_index:     .res 1     ; Current index of the OAM buffer
+    controller:    .res 1     ; Controller input
+    x_pos:         .res 2     ; X position of the player (LSB: subpixel position, MSB: pixel position)
+    y_pos:         .res 2     ; Y position of the player (LSB: subpixel position, MSB: pixel position)
+    x_vel:         .res 2     ; X velocity of the player (LSB: subpixel velocity, MSB: pixel velocity)
+    y_vel:         .res 2     ; Y velocity of the player (LSB: subpixel velocity, MSB: pixel velocity)
 
-.segment "RAM"              ; The rest of internal RAM after ZP, the OAM buffer, and the VRAM buffer
-.segment "PRG_RAM"          ; Extra cartridge RAM
+.segment "RAM"                ; The rest of internal RAM after ZP, the OAM buffer, and the VRAM buffer
+    tilemap:       .res 240   ; A map that stores which tiles are solid
+.segment "PRG_RAM"            ; Extra cartridge RAM
